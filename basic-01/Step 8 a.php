85367c75bd7fed57b8f822256141dfff5a5cb30d
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-05-23 21:23:41 +0300

    STep 8 a

---
+++ basic-01/Step 8 a.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$menu = array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+echo "Hello Sir, do you have " . $menu[0] . ", " . $menu[1] . " and " . $menu[2] . "? Also if you sell fruits can I find a " . $menu[5] . "?";
+
+
+?>
