01ffe5e78a916a0bad608cccfeb1293addb44361
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-05-23 21:07:25 +0300

    Step 7

---
+++ basic-01/Step 7.php
@@ -0,0 +1,12 @@
+<?php
+
+$birth_year = 1990;
+$current_year = 2021;
+$age = $current_year - $birth_year;
+
+if ($age > 18){
+    echo "You can drive";
+} else {
+  echo "You still a kid, go and play GTA";
+}
+?>
