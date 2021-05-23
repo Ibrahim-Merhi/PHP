479f44a798252b0e2ac9b03e7276521d0f701194
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-05-23 20:28:37 +0300

    Step 4

---
+++ basic-01/Step 4.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = "Gaby";
+$position = "tech consultant";
+$github_url = "https://github.com/gk-git";
+echo "Hello, My name is $name, "."I am a  $position,"." Please check my github link $github_url </br>";
+echo "Hello, My name is $name, I am a  $position, Please check my github link $github_url </br>";
+echo 'Hello, My name is '.$name.', I am a '.$position.', Please check my github link '.$github_url;
+
+?>
