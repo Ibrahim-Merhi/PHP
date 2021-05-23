ff4ff3aa5498df984f55b9994f6d9ddbfd5c558c
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-05-23 22:11:33 +0300

    Step 8 b

---
+++ basic-01/Step 8 b.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$menu = array("Eggs"=>array("Balade", "Mazere3"), "Milk"=>array("Fresh", "Taanayel"), "Water-Pack" =>array("Tanoureen", "Reem"));
+echo "Hey Sir, Please I need 1 pack of " . $menu["Eggs"][0] . " Eggs and 3 " . $menu["Water-Pack"][1] . " Water Pack ";
+
+
+?>
