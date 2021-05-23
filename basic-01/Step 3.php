2a3207766e1097825ef69f9bf86cadf9a57a21f4
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-05-23 19:29:26 +0300

    Step 3

---
+++ basic-01/Step 3.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = "Ibrahim";
+$email = "ibraim.m.merhy@gmail.com";
+echo "My name is $name and my email is $email </br>";
+
+$height = 20;
+$width = 10;
+$area = $height*$width;
+echo "The are is $area";
+
+?>
