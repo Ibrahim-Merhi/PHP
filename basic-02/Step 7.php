a787e3b620e69fe67285052deb98665671a9ba7a
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2021-05-24 12:53:24 +0300

    Step 7

---
+++ basic-02/Step 7.php
@@ -0,0 +1,8 @@
+<?php
+$text = "Python is more popular than PHP" ;
+echo "The original sentence: </br>";
+echo "Python is more popular than PHP </br>";
+echo "The Replaced sentence: </br>";
+echo str_replace("Python","PHP",$text);
+?> 
+
